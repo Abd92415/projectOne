f314577ad786873fffc137c71e0780cd08a80f73
Abd92415 <Your github emailAbd92415>
2023-11-21 00:21:29 +0300

    DART Shop

---
+++ lib/services/constant/path_images.dart
@@ -0,0 +1,6 @@
+class PathImage {
+  static const String splashPhoto = 'assets/images/logoSp.png';
+  static const String rigeter = 'assets/images/rigester.jpg';
+  static const String userImage = 'assets/images/bgColor.jpg';
+  static const String postEmpty = 'assets/images/list.png';
+}
